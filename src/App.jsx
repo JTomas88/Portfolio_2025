15eecd759dac06a4f2d8f2c35fcb7edbb49d8ce6
Juan Tomas <juan.tomas88@hotmail.com>
2025-07-01 22:44:17 +0200

    terminado home. TODO: Formacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import './App.css'
 import { Footer } from './components/Footer/Footer';
+import Formacion from './pages/Formacion/Formacion';
 
 function App() {
 
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-      </Routes>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/formacionycursos" element={<Formacion />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,53 +1,116 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+
+import React, { useState } from "react";
+import { useNavigate, } from "react-router-dom";
 import fotoPerfil from '../../assets/fotoPerfil.png'
 import "./home.css";
 
 export default function Home() {
     const navigate = useNavigate();
 
-    return (
-        <div className="w-full h-screen home_container text-center ">
-            <div className="text-center">
-                <h1 className="titulo" >Juan Tomás Poves</h1>
-                <h3 className="subtitulo">Fullstack developer</h3>
-            </div>
-            <div className="flex justify-center">
-                <div >
-                    <img
-                        src={fotoPerfil}
-                        className="rounded-md m-4  mx-auto block fotoPerfil"
-                        alt="Foto de perfil"
-                    />
-                    <p className="font-semibold text-black">
 
-                        Apasionado desarrollador full stack con un sólido dominio de tecnologías como React, Python y SQL.<br />
-                        Durante trabajos anteriores he adquirido habilidades de comunicación, resolución de problemas o escucha activa.<br />
-                        Busco contribuir al desarrollo y éxito de proyectos que mejoren la vida de las personas. <br />
-                        Autodidacta, comprometido con la excelencia y en constante aprendizaje, deseo aplicar mis conocimientos al desarrollo web.
-                    </p>
-                    <div className="flex flex-wrap justify-center gap-50 mt-15">
-                        <button
-                            type="button"
-                            className="botones flex items-center justify-center"
-                            onClick={() => navigate("/formacion")}
-                        >
-                            Formación / Cursos
-                        </button>
-                        <button type="button" className="botones flex items-center justify-center">
-                            Experiencia Laboral
-                        </button>
-                        <button type="button" className="botones flex items-center justify-center">
-                            Proyectos
-                        </button>
-                        <button type="button" className="botones flex items-center justify-center">
-                            Tecnologías
-                        </button>
+    return (
+        <div className="w-full min-h-screen home_container text-center py-8 px-4 flex flex-col items-center">
+            <main className="w-full max-w-5xl mt-10 flex flex-col items-center gap-6">
+                <div className="text-center">
+                    <h1 className="titulo" >Juan Tomás Poves</h1>
+                    <h3 className="subtitulo">Fullstack developer</h3>
+                </div>
+                <div className="flex justify-center">
+                    <div >
+                        <img
+                            src={fotoPerfil}
+                            className="rounded-md m-4  mx-auto block fotoPerfil"
+                            alt="Foto de perfil"
+                        />
+                        <p className="font-semibold text-black">
+
+                            Apasionado desarrollador full stack con un sólido dominio de tecnologías como React, Python y SQL.<br />
+                            Durante trabajos anteriores he adquirido habilidades de comunicación, resolución de problemas o escucha activa.<br />
+                            Busco contribuir al desarrollo y éxito de proyectos que mejoren la vida de las personas. <br />
+                            Autodidacta, comprometido con la excelencia y en constante aprendizaje, deseo aplicar mis conocimientos al desarrollo web.
+                        </p>
+                        <div className="flex flex-wrap justify-center items-center gap-10 mt-8 mb-12 overflow-x-auto botones-wrapper">
+
+                            <button
+                                className="bubble"
+                                onClick={() => navigate("/formacionycursos")}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    textAlign: 'center'
+                                }}
+                            >
+                                <span>Formación</span>
+                                <span style={{ fontWeight: 'bold', margin: '0.1rem 0' }}>y</span>
+                                <span>Cursos</span>
+                            </button>
+
+                            <button
+                                className="bubble"
+                                onClick={() => navigate("/formacion")}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    textAlign: 'center'
+                                }}
+                            >
+                                Experiencia laboral
+                            </button>
+
+                            <button
+                                className="bubble"
+                                onClick={() => navigate("/formacion")}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    textAlign: 'center'
+                                }}
+                            >
+                                Proyectos
+                            </button>
+
+                            <button
+                                className="bubble"
+                                onClick={() => navigate("/formacion")}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    textAlign: 'center'
+                                }}
+                            >
+                                Tecnologías
+                            </button>
+
+                            {/* <div className="buble">
+                                <button type="button" className="botones">
+                                    Experiencia Laboral
+                                </button>
+                            </div>
+                            <div className="buble">
+                                <button type="button" className="botones">
+                                    Proyectos
+                                </button>
+                            </div>
+                            <div className="bubble">
+                                <button type="button" className="botones">
+                                    Tecnologías
+                                </button>
+                            </div> */}
+                        </div>
+
+
+
                     </div>
 
                 </div>
+            </main>
 
-            </div>
         </div >
     );
-};
+}
+
+
